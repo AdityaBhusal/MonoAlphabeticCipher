3f12fe3d19549af6368d284d089ca212f97ba9f6
Aditya <jusadyee@gmail.com>
2024-05-13 23:33:26 +0545

    first commit on this repo

---
+++ MonoalphabeticCipher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program{
+
+    public static void Main (string[] args){
+        Console.WriteLine("Enter the Plain Text to be encrypted: ");
+        string userInput  = Console.ReadLine()??"text not entered, ";
+        string CipherKey="WXZYLKJHGUFTBSQPENOMCRDIAV";
+        string CipherText="";
+        foreach (char i in userInput)
+        {
+            int a = (int)i -97;
+            CipherText += CipherKey[a];
+        }
+        Console.WriteLine("The Encrypted Text is: "+CipherText);
+
+}
+}
